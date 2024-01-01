6abcf42e5cd2ac6647baee653d1b160330061397
Crypto <iamsuryavamsi@gmail.com>
2024-01-01 14:10:26 +0530

    Changes Done

---
+++ JS/Rock_Paper_Scissor.js
@@ -0,0 +1,127 @@
+let score = JSON.parse(localStorage.getItem(`score`))|| {
+    wins: 0,
+    looses: 0,
+    ties: 0
+}
+
+function computerChoosed(){
+
+    let computerPickedValue = ``;
+
+    const randomNumber = Math.random();
+
+    if (randomNumber > 0 && randomNumber <= 1 / 3){
+
+        computerPickedValue = `Rock`;
+
+    } else if(randomNumber > 1 / 3 && randomNumber <= 2 / 3){
+
+        computerPickedValue = `Paper`;
+
+    } else if(randomNumber > 2 / 3 && randomNumber <= 1){
+
+        computerPickedValue = `Scissors`;
+
+    } 
+
+    return computerPickedValue;
+
+}
+
+function playGame(playerPicked){
+
+    const computerPicked = computerChoosed();
+    
+    let result = ``;
+
+    if (playerPicked === `Rock`){
+
+        if (computerPicked === `Rock`){
+
+            result = `Tie`;
+
+        } else if (computerPicked === `Paper`){
+
+            result = `You Loose`;
+
+        } else if (computerPicked === `Scissors`){
+
+            result = `You Win`;
+
+        }
+
+    } else if (playerPicked === `Paper`){
+
+        if (computerPicked === `Rock`){
+
+            result = `Tie`;
+
+        } else if (computerPicked === `Paper`){
+
+            result = `You Loose`;
+
+        } else if (computerPicked === `Scissors`){
+
+            result = `You Win`;
+
+        }
+        
+    } else if (playerPicked === `Scissors`){
+
+        if (computerPicked === `Rock`){
+
+            result = `Tie`;
+
+        } else if (computerPicked === `Paper`){
+
+            result = `You Loose`;
+
+        } else if (computerPicked === `Scissors`){
+
+            result = `You Win`;
+
+        }
+
+    }
+
+    if (result === `You Win`){
+
+        score.wins ++;
+
+    } else if (result === `You Loose`){
+
+        score.looses ++;
+
+    } else if (result === `Tie`){
+
+        score.ties ++;
+
+    }
+
+    localStorage.setItem(`score`, JSON.stringify(score));
+
+    document.querySelector(`.div3_01`)
+        .innerHTML = `${result}`;
+
+    document.querySelector(`.div3_02`)
+        .innerHTML = `Your <img class="icon_01" src="/Project_Rock_Paper/IMAGES/${playerPicked}-emoji.png"> - Computer's <img class="icon_01" src="/Project_Rock_Paper/IMAGES/${computerPicked}-emoji.png">`;
+
+    document.querySelector(`.div3_03`)
+        .innerHTML = ` Wins:- ${score.wins}, Looses:- ${score.looses}, Ties:- ${score.ties}`;
+
+}
+
+function removeScore(){
+
+    score.wins = 0;
+    
+    score.looses = 0;
+
+    score.ties = 0;
+
+    localStorage.removeItem(`score`);
+
+    document.querySelector(`.div3_03`)
+        .innerHTML = ` Wins:- ${score.wins}, Looses:- ${score.looses}, Ties:- ${score.ties}`;
+
+}
