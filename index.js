e66ee6bc0b213395d1ee1211d9db01489c9508fe
Crypto <iamsuryavamsi@gmail.com>
2024-01-09 16:18:47 +0530

    Changes Done

--- index.js
+++ index.js
@@ -19,3 +19,33 @@ const hamburgerFunction = () => {
 hamburger.addEventListener("click", () => {
     hamburgerFunction();
 })
+
+function filterButton(argu01){
+
+    const filterButton01 = document.querySelector(argu01)
+
+    if (!filterButton01.classList.contains('isActive')){
+
+        filter();
+
+        filterButton01.classList.add('isActive')
+
+    } else {
+
+        filterButton01.classList.remove('isActive')
+
+    }
+
+}
+
+function filter(){
+
+    const filterElement = document.querySelector('.isActive');
+
+    if (filterElement){
+
+        filterElement.classList.remove("isActive");
+
+    } 
+
+}
